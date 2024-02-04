d79cbbb754b69356fbace6000ab289fd3a7b5a72
benevos <b4dshot_assassin@hotmail.com>
2024-02-04 13:37:12 -0600

    fix: logo button turned into link

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -4,6 +4,7 @@ import Logo from '../Logo';
 
 import Navbar from './Navbar/Navbar';
 import OpenMobileMenuButton from './Navbar/OpenMobileMenuButton';
+import Link from 'next/link';
 
 function Header() 
 {
@@ -12,12 +13,12 @@ function Header()
         <header className='h-[75px] mt-[25px] flex items-center justify-between
                            max-home-md:mt-0'>
             
-          <button className='h-full  pl-[30px]'>
+          <Link className='h-full pl-[30px] flex items-center justify-center' href={'/'}>
             <div className='w-[40px] h-[40px]'>
                 <Logo/>
             </div>
               
-          </button>
+          </Link>
 
           <Navbar/>
 
